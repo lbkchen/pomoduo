ecbc720bb7b69f17d8a3de2185454e1b8564b374
Ken Chen <ken.chen@samsara.com>
2019-03-02 11:50:09 -0800

    client.py: Remove server socket, add client socket
    Realized that the Flask-SocketIO implementation was actually for
    the socket-io server, so we're using python-socketio now for
    the client implementation.

--- client.py
+++ client.py
@@ -1,20 +1,18 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+import asyncio
+import socketio
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+sio = socketio.AsyncClient()
+
+
+@sio.on("message")
+async def on_message(data):
+    print("I received a message!")
 
 
 @app.route('/')
 def index():
     return render_template('./index.html')
-
-
-@socketio.on('my event')
-def test_message(message):
-    emit('my response', {'data': 'got it!'})
-
-
-if __name__ == '__main__':
-    socketio.run(app)
