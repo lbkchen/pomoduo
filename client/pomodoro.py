8b9d630e68b5beeb97dc2eeba412b2c574fac5ff
Ken Chen <ken.chen@samsara.com>
2019-03-16 19:06:52 -0700

    refactor pomodoro.

--- client/pomodoro.py
+++ client/pomodoro.py
@@ -5,27 +5,49 @@ from enum import Enum
 from math import floor
 
 
-class PomodoroState(Enum):
-    INACTIVE = 0
-    WORKING = 1
-    RESTING = 2
-    YIELD_FOR_CONTINUE = 3
+class PomodoroState:
+    DURATION_SECS = 100
+
+    def __init__(self):
+        self.name = "invalid"
+        self.duration_secs = self.DURATION_SECS
+
+    def __repr__(self):
+        return "<PomodoroState.%s>" % self.name.upper()
+
+    def __str__(self):
+        return self.__repr__()
+
+
+class YieldState(PomodoroState):
+    DURATION_SECS = float("inf")
+
+    def __init__(self):
+        self.name = "inactive"
+
+
+class WorkingState(PomodoroState):
+    DURATION_SECS = 25 * 60
+
+    def __init__(self, duration_secs=DURATION_SECS):
+        self.name = "working"
+        self.duration_secs = duration_secs
+
+
+class RestingState(PomodoroState):
+    DURATION_SECS = 5 * 60
+
+    def __init__(self, duration_secs=DURATION_SECS):
+        self.name = "resting"
+        self.duration_secs = duration_secs
 
 
 class Pomodoro:
-    WORK_TIME_SEC = 25 * 60
-    REST_TIME_SEC = 5 * 60
     POLLING_INTERVAL_SEC = 1
 
-    STATE_INTERVALS = {
-        PomodoroState.INACTIVE: float("inf"),
-        PomodoroState.WORKING: WORK_TIME_SEC,
-        PomodoroState.RESTING: REST_TIME_SEC,
-        PomodoroState.YIELD_FOR_CONTINUE: float("inf"),
-    }
-
     def __init__(self, poll_callback=None, debug=False):
-        self.state = PomodoroState.INACTIVE
+        self.state = YieldState()
+        self.timer = None
         self.start_time = time.time()
         self.poll_callback = poll_callback
         self.polling_timer = Timer(self.POLLING_INTERVAL_SEC, self.poll)
@@ -39,30 +61,46 @@ class Pomodoro:
         return round(time.time() - self.start_time)
 
     @property
+    def remaining(self):
+        return self.state.duration_secs - self.elapsed
+
+    @property
     def info(self):
         return {
-            "state": self.state.value,
+            "state": str(self.state),
             "elapsed": self.elapsed,
+            "remaining": self.remaining,
         }
 
-    def start(self):
-        self.state = PomodoroState.WORKING
-        self.initialize_timer()
-
-        self.timer = Timer(self.WORK_TIME_SEC, self.handle_yield)
+    def begin_current_state(self, on_finish=None):
+        if self.timer:
+            self.timer.cancel()
+        if not on_finish:
+            on_finish = self.begin_yielding
+        self.timer = Timer(self.state.duration_secs, on_finish)
         self.timer.start()
-
-    def initialize_timer(self):
         self.start_time = time.time()
-        self.end_time = None
+
+    def begin_yielding(self):
+        self.state = YieldState()
+        if self.timer:
+            self.timer.cancel()
+        self.timer = None
+
+    def begin_working(self):
+        self.state = WorkingState()
+        self.begin_current_state(self.begin_resting)
+
+    def begin_resting(self):
+        self.state = RestingState()
+        self.begin_current_state(self.begin_yielding)
 
     def poll(self):
         # Handle poll first
         self.handle_poll()
 
         # Repeat timer infinitely by resetting upon completion
-        if self.polling_timer.is_alive():
-            self.polling_timer.cancel()
+        self.polling_timer.cancel()
         self.polling_timer = Timer(self.POLLING_INTERVAL_SEC, self.poll)
         self.polling_timer.start()
 
@@ -75,9 +113,5 @@ class Pomodoro:
             self.poll_callback(self.info)
 
         # Check for state change
-        current_interval = self.STATE_INTERVALS[self.state]
-        if self.elapsed >= current_interval:
+        if self.remaining <= 0:
             print("Need to change state.")
-
-    def handle_yield(self):
-        pass
